d276c19fe6eb9b8625a1881b31307f355ac3a4c4
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2024-07-02 17:40:11 +0530

    hosted both projects

---
+++ basicreact01/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Chai aur React | Mehreen Zahid</h1>
+  );
+}
+
+export default App;
---
+++ vitereact01/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <h1>Chai aur React with Vite | Mehreen Zahid</h1>
+  )
+}
+
+export default App
